a41ec25c482e09c2d8e06b437378643665f48dc2
RenderMan <render@renderlab.net>
2016-10-17 05:35:54 -0600

    Bug Fixes

--- gentempgraph.sh
+++ gentempgraph.sh
@@ -3,30 +3,40 @@
 DATA_Dir=/home/pi/Data
 
 TODAY=`date "+%m/%d/%y %H:%M:%S"`
-YESTERDAY=`date "+%m/%d/%y %H:%M:%S" --date="-48 hour"`
+YESTERDAY=`date "+%m/%d/%y %H:%M:%S" --date="-1 day"`
+DAILY=`date "+%m/%d/%y %H:%M:%S" --date="-12 hour"`
 WEEK=`date "+%m/%d/%y" --date="-7 day"`
 MONTH=`date "+%m/%d/%y" --date="-1 month"`
 
 gnuplot <<-EOF
 
-set term png size 1024, 1024
+set term png size 1024, 2048
 set output '$DATA_Dir/Temp.png'
 #unset key
 set key left top
 set ylabel 'Temperature (Celsius)'
 set border 3
 set xtics nomirror
+set xtics rotate
 set ytics nomirror
 unset xlabel
-set multiplot layout 3, 1 title "Temperatures"
+set multiplot layout 4, 1 title "Temperatures"
 
-                set timefmt "%m/%d/%y %H:%M:%S"
+		set title 'By Hour'
                 set xdata time
-                set title 'Monthly'
-                set format x "%m/%d/%y"
-		set xrange ["$MONTH":"$TODAY"]
+                set timefmt "%m/%d/%y %H:%M:%S"
+                set format x "%m/%d/%y %H:%M:%S"
                 set xtics autofreq
-                plot '$DATA_Dir/Int_temp.dat' u 3:2 lc 2 smooth sbezier, '$DATA_Dir/Int_temp.dat' every 300 u 3:2 w points lc 2 pt 1 ps 1, '$DATA_Dir/Ext_temp.dat' u 3:2 lc 1 smooth sbezier, '$DATA_Dir/Ext_temp.dat' every 300 u 3:2 w points lc 1 pt 1 ps 1
+                set xrange ["$DAILY":"$TODAY"]
+		plot '$DATA_Dir/Int_temp.dat' u 3:2 t 'Interior Temp Profile' lc 2 smooth sbezier, '$DATA_Dir/Int_temp.dat' every 300 u 3:2 t 'Interior Temp Points' w points lc 2 pt 1 ps 1, '$DATA_Dir/Ext_temp.dat' u 3:2 t 'Exterior Temp Profile' lc 1 smooth sbezier, '$DATA_Dir/Ext_temp.dat' every 300 u 3:2 t 'Exterior Temp Points' w points lc 1 pt 1 ps 1
+
+                set title 'By Day'
+                set xdata time
+                set timefmt "%m/%d/%y"
+                set format x "%m/%d/%y"
+                set xtics autofreq
+                set xrange ["$YESTERDAY":"$TODAY"]
+                plot '$DATA_Dir/Int_temp.dat' u 3:2 t 'Interior Temp Profile' lc 2 smooth sbezier, '$DATA_Dir/Int_temp.dat' every 300 u 3:2 t 'Interior Temp Points' w points lc 2 pt 1 ps 1, '$DATA_Dir/Ext_temp.dat' u 3:2 t 'Exterior Temp Profile' lc 1 smooth sbezier, '$DATA_Dir/Ext_temp.dat' every 300 u 3:2 t 'Exterior Temp Points' w points lc 1 pt 1 ps 1
 
                 set title 'By Week'
 		set xdata time
@@ -34,15 +44,15 @@ set multiplot layout 3, 1 title "Temperatures"
                 set format x "%m/%d/%y"
                 set xtics autofreq
                 set xrange ["$WEEK":"$TODAY"]
-                plot '$DATA_Dir/Int_temp.dat' u 3:2 lc 2 smooth sbezier, '$DATA_Dir/Int_temp.dat' every 25 u 3:2 w points lc 2 pt 1 ps 1, '$DATA_Dir/Ext_temp.dat' u 3:2 lc 1 smooth sbezier, '$DATA_Dir/Ext_temp.dat' every 25 u 3:2 w points lc 1 pt 1 ps 1
+		plot '$DATA_Dir/Int_temp.dat' u 3:2 t 'Interior Temp Profile' lc 2 smooth sbezier, '$DATA_Dir/Int_temp.dat' every 300 u 3:2 t 'Interior Temp Points' w points lc 2 pt 1 ps 1, '$DATA_Dir/Ext_temp.dat' u 3:2 t 'Exterior Temp Profile' lc 1 smooth sbezier, '$DATA_Dir/Ext_temp.dat' every 300 u 3:2 t 'Exterior Temp Points' w points lc 1 pt 1 ps 1
 
-		set title 'By Day'
+		set timefmt "%m/%d/%y %H:%M:%S"
                 set xdata time
-                set timefmt "%m/%d/%y"
+                set title 'Monthly'
                 set format x "%m/%d/%y"
+                set xrange ["$MONTH":"$TODAY"]
                 set xtics autofreq
-                set xrange ["$YESTERDAY":"$TODAY"]
-                plot '$DATA_Dir/Int_temp.dat' u 3:2 lc 2 smooth sbezier, '$DATA_Dir/Int_temp.dat' every 2 u 3:2 w points lc 2 pt 1 ps 1, '$DATA_Dir/Ext_temp.dat' u 3:2 lc 1 smooth sbezier, '$DATA_Dir/Ext_temp.dat' every 2 u 3:2 w points lc 1 pt 1 ps 1
+		plot '$DATA_Dir/Int_temp.dat' u 3:2 t 'Interior Temp Profile' lc 2 smooth sbezier, '$DATA_Dir/Int_temp.dat' every 300 u 3:2 t 'Interior Temp Points' w points lc 2 pt 1 ps 1, '$DATA_Dir/Ext_temp.dat' u 3:2 t 'Exterior Temp Profile' lc 1 smooth sbezier, '$DATA_Dir/Ext_temp.dat' every 300 u 3:2 t 'Exterior Temp Points' w points lc 1 pt 1 ps 1
 
 EOF
 
--- gettemp.sh
+++ gettemp.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-Data_dir=/home/pi/Data
 
-Int_temp=`/usr/local/bin/temper-poll |grep 'Device #0'|gawk '{print $3}'|sed 's/°C//'`
-Ext_temp=`/usr/local/bin/temper-poll |grep 'Device #1'|gawk '{print $3}'|sed 's/°C//'`
+#Set the directory where to store the data
+Data_dir=/home/pi/Data/
+
+#Query the temperature sensors, get each device, strip the temperature out and drop the notation
+Int_temp=`/usr/local/bin/temper-poll |grep 'Device #1'|gawk '{print $3}'|sed 's/°C//'`
+Ext_temp=`/usr/local/bin/temper-poll |grep 'Device #0'|gawk '{print $3}'|sed 's/°C//'`
+
+#Echo the date and time into the log with the temperature
 when=`date +%s`
 thetime=`date +%D' '%T`
 #if [ -n "$Int_temp" ]; then
         echo $when $Int_temp $thetime>> $Data_dir/Int_temp.dat
         echo $when $Ext_temp $thetime>> $Data_dir/Ext_temp.dat
 #fi
-sed -i '/\<77.3\>/d' $Data_dir/Int_temp.dat
-sed -i '/\<77.3\>/d' $Data_dir/Ext_temp.dat
+
+#Ocassionally the sensor gives a bogus reading, so strip out values of 77.3 from the logs
+cd $Data_dir
+sed -i '/\<77.3\>/d' Int_temp.dat
+sed -i '/\<77.3\>/d' Ext_temp.dat
